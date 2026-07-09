17622262edd3548e227e99a39801e7e54ed9160c
Prakhar Gupta <145137890+prakhar2204@users.noreply.github.com>
2026-07-09 22:10:41 +0530

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Using environment variables directly
-const supabaseUrl = 'https://ydgfhnrdksygeunnardq.supabase.co/rest/v1/';
+const supabaseUrl = 'https://ydgfhnrdksygeunnardq.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZ2ZobnJka3N5Z2V1bm5hcmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDU0OTAsImV4cCI6MjA5OTE4MTQ5MH0.zJJYyzt10Q6VaNzHtTBZvfEh6vjU_C5Doqerat0VQXQ';
 
 if (!supabaseUrl || !supabaseAnonKey) {
